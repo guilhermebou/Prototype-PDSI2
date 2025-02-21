a15d528f4c52ba9337ff86c248a3009cd9894c69
Guilherme Bou <guilherme_bou@hotmail.com>
2025-02-21 14:30:32 -0300

    add front

--- main.py
+++ main.py
@@ -3,9 +3,25 @@ import classes
 import model
 from database import engine, get_db
 from sqlalchemy.orm import Session
+from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+
 
 model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+origins =[
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 @app.get("/")
 def read_root():
     return {"Hello": "lala"}
@@ -17,3 +33,8 @@ def criar_valores(nova_mensagem: classes.Mensagem, db: Session=Depends(get_db)):
     db.commit()
     db.refresh(mensagem_criada)
     return{"Mensagem": mensagem_criada}
+
+@app.get("/mensagens", response_model=List[classes.Mensagem], status_code=status.HTTP_200_OK)
+async def buscar_valores(db: Session = Depends(get_db), skip: int=0, limit: int=100):
+    mensagens = db.query(model.Model_Mensagem).offset(skip).limit(limit).all()
+    return mensagens
