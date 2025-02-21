b672722b093e1b07a76f2736c5f5dd2d159e060a
Guilherme Bou <guilherme_bou@hotmail.com>
2025-02-21 11:36:43 -0300

    teste ação

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ model.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 @app.get("/")
 def read_root():
-    return {"Hello": "lalalalalalalala"}
+    return {"Hello": "lalalala"}
 
 @app.post("/criar", status_code=status.HTTP_201_CREATED)
 def criar_valores(nova_mensagem: classes.Mensagem, db: Session=Depends(get_db)):
